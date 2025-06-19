f7eb204e5a34fe46807aa0a05eddc586a5024d2e
Ann K <kubareva.ann@gmail.com>
2025-06-19 14:06:52 +0300

    fixed tasks 3 and 6

--- src/task-6.ts
+++ src/task-6.ts
@@ -20,6 +20,6 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]);
-getFirstElement(["a", "b", "c"]);
-getFirstElement([true, false, true]);
+getFirstElement<number>([1, 2, 3]);
+getFirstElement<string>(["a", "b", "c"]);
+getFirstElement<boolean>([true, false, true]);
